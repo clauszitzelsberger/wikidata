5e3a16a23388be3a5341b1412e2d0ad2bd6a877e
Claus Zitzelsberger <40275652+clauszitzelsberger@users.noreply.github.com>
2022-01-03 17:09:43 +0100

    Update script.py

--- script.py
+++ script.py
@@ -2,6 +2,25 @@
 from qwikidata.sparql import return_sparql_query_results
 import time
 
+wikidata_properties = {
+    "instance_of": "wdt:P31",
+    "continent": "wdt:P30",
+    "country": "wdt:P17"}
+
+wikidata_items = {
+    "sovereign_state": "wd:Q3624078",
+    "big city": "wd:Q1549591",
+
+    # continents
+    "africa": "wd:Q15",
+    "south america": "wd:Q18",
+    "europe": "wd:Q46",
+    "asia": "wd:Q48",
+    "north america": "wd:Q49",
+    "oceania": "wd:Q538"
+}
+
+
 ### Sparql Queries
 big_cities_country = """
 select ?itemLabel ?countryLabel
@@ -13,8 +32,6 @@ where {
   ?item wdt:P17 ?country .
   ?country rdfs:label ?countryLabel .
   filter(lang(?countryLabel) = "en") .
-
-  %s
 
   ?item wdt:P17 wd:%s
   }
@@ -29,7 +46,7 @@ def standard_query(item, prop, prop_of_property=None, prop_item=None, limit=None
 
     E.g.
     SELECT *
-    WHERE ?item is instance of mountains
+    WHERE ?item is instance of big cities
     AND ?item sovereign state of this item ?property
     AND ?property continent of which the subject is a part is africa
     """
@@ -81,7 +98,7 @@ def queryWikidata(query):
      
 if __name__ == '__main__':
     country_query = standard_query("sovereign_state", "continent")
-    countries = queryWikidata(country_query)
+    countries = queryWikidata(country_query) #returns list of dictionaries
 
     # Count iterations
     i=0
@@ -94,11 +111,14 @@ if __name__ == '__main__':
     # Iterate over countries
     for country in countries:
         country_name = country['itemLabel']['value']
+        print(country_name)
         
         # Get all cities
+        wikidata_country_tag = country['item']['value'][31:] # Wikidata wd:? tag for given country
+        cities = queryWikidata(big_cities_country % (wikidata_country_tag))
         
-        # Wikidata wd:? tag for given country
-        wikidata_country_tag = country['item']['value'][31:]
-        cities = queryWikidataWrapper(big_cities_country % ('', wikidata_country_tag))
-        
-        print(cities)
+        if len(cities)==0:
+            continue
+        else:
+            for city in cities:
+                print(city['itemLabel']['value'])
